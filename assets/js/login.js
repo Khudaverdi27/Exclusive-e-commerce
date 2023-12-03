fface9202fd927cd82c0e356c5334800ada4b0f8
Xudaverdi <xudaverdi.sh@gmail.com>
2023-12-04 02:45:10 +0400

    CREATE ACCOUNT PREPEARING

--- assets/js/login.js
+++ assets/js/login.js
@@ -12,7 +12,7 @@ const loginPage = () => {
                 <p class="ms-3">Enter your details below</p>
                 <form class="inputsAndBtns" name="form">
                     <div class="inputs d-flex flex-column position-relative">
-                        <input name="email" type="text" class="borderlessInput mt-3 mb-1 py-2 ps-1" placeholder="Email or Phone Number">
+                        <input data-email="emailLogin" name="email" type="text" class="borderlessInput mt-3 mb-1 py-2 ps-1" placeholder="Email or Phone Number">
                         <div id="email_error" class='fs-12 text-danger bg-light-red rounded px-2 d-none'>Please fill up your e-mail or phone number</div>
                         <input id="inputPassLoginPage" name="password" type="password" class="borderlessInput mt-1 mb-1 w-100 py-2 ps-1" placeholder="Password">
                         <i id="eyeIconLogin" class="position-absolute fs-5 text-secondary fa-solid fa-eye-slash"></i>
@@ -34,65 +34,8 @@ const loginPage = () => {
                 email_error: document.getElementById('email_error')
             };
 
-            selectors.email.addEventListener('input', email_verify);
-            selectors.pass.addEventListener('input', pass_verify);
 
-            selectors.form.addEventListener('submit', (e) => {
-                e.preventDefault();
-                validateForm();
-            });
-
-            const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-            const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
-
-            function validateForm() {
-
-                if (!validateInput(selectors.email, selectors.email_error, emailRegex)) {
-                    return // else; 
-                }
-
-                if (!validateInput(selectors.pass, selectors.pass_error, passwordRegex)) {
-                    return;
-                }
-
-                const value = Boolean(true);
-                const queryString = new URLSearchParams(value).toString()
-
-                const spinner = document.querySelector('.loader-card')
-                spinner.classList.remove('d-none')
-                setTimeout(() => {
-                    spinner.classList.add('d-none')
-                    window.location.href = "header.html?" + queryString;
-                }, 3000)
-
-
-
-            }
-            // form input class changes
-            function validateInput(input, errorElement, regex) {
-                if (!regex.test(input.value)) {
-                    input.classList.add('border', 'border-danger');
-                    errorElement.classList.remove('d-none');
-                    input.focus();
-                    return false;
-                } else {
-                    input.classList.remove('border', 'border-danger');
-                    errorElement.classList.add('d-none');
-
-                    return true;
-
-                }
-
-            }
-
-            function email_verify() {
-                validateInput(selectors.email, selectors.email_error, emailRegex);
-            }
-
-            function pass_verify() {
-                validateInput(selectors.pass, selectors.pass_error, passwordRegex);
-            }
-
+            selectElement(selectors.form, selectors.email, selectors.email_error, selectors.pass, selectors.pass_error)
 
         });
     }
@@ -117,9 +60,112 @@ function showAndHidePassword(eyeIconId, passInputId) {
 
 }
 
+loginPage();
+
+
+
+
+
+function selectElement(form, email, email_error, pass, pass_error, nameInput, name_error) {
+
+    const emailRegex = /^(?:[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}|[0-9()+-\s]+)$/
+    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+    const nameRegex = /^[a-zA-Z0-9_\-'.()&!*$][a-zA-Z0-9\s_\-'.()&!*$]*[a-zA-Z0-9_\-'.()&!*$]$/;
+    if (email) {
+        email.addEventListener('input', email_verify);
+    }
+    if (pass) {
+        pass.addEventListener('input', pass_verify);
+    }
+
+    if (nameInput) {
+        nameInput.addEventListener('input', name_verify);
+    }
+    if (form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            validateForm();
+        });
+    }
+
+    function validateForm() {
+        if (nameInput) {
+            if (!validateInput(nameInput, name_error, nameRegex)) {
+                return;// else; 
+            }
+        }
+
+        if (!validateInput(email, email_error, emailRegex)) {
+            return;// else; 
+        }
+
+        if (!validateInput(pass, pass_error, passwordRegex)) {
+            return;// else; 
+        }
+
+
+        const value = Boolean(true);
+        const queryString = new URLSearchParams(value).toString()
+
+        const spinner = document.querySelector('.loader-card')
+        spinner.classList.remove('d-none')
+        setTimeout(() => {
+            spinner.classList.add('d-none')
+            window.location.href = "header.html?" + queryString;
+        }, 3000)
+
+
+
+    }
+    // form input class changes
+    function validateInput(input, errorElement, regex) {
+        if (!regex.test(input.value)) {
+            input.classList.add('border', 'border-danger');
+            errorElement.classList.remove('d-none');
+            input.focus();
+            return false;
+        } else {
+            input.classList.remove('border', 'border-danger');
+            errorElement.classList.add('d-none');
+
+            return true;
+
+        }
+
+    }
+
+    function email_verify() {
+        validateInput(email, email_error, emailRegex);
+    }
+
+    function pass_verify() {
+        validateInput(pass, pass_error, passwordRegex);
+    }
+    function name_verify() {
+        validateInput(nameInput, name_error, nameRegex);
+    }
+
+
+
+}
 
 
 // from html
-showAndHidePassword('eyeIconAccountPage', 'passInputAccountPage')
+showAndHidePassword('eyeIconAccountPage', 'input-pass-sign')
 
-loginPage();
+
+const uiHtml = {
+    form: document.getElementById('form-sign'),
+    name: document.getElementById('input-name-sign'),
+    name_error: document.getElementById('signName_error'),
+    email: document.getElementById('input-email-sign'),
+    pass: document.getElementById('input-pass-sign'),
+    pass_error: document.getElementById('signPass_error'),
+    email_error: document.getElementById('signEmail_error')
+}
+
+
+selectElement(uiHtml.form, uiHtml.email, uiHtml.email_error,
+    uiHtml.pass, uiHtml.pass_error, uiHtml.name, uiHtml.name_error)
+
+
--- assets/js/spinner.js
+++ assets/js/spinner.js
@@ -1,1 +1,1 @@
-document.querySelector('.loader-card').innerHTML = '<img src="./assets/images/other/loader.gif" class="card-img-top rounded " alt="loading">'
+document.querySelector('.loader-card').innerHTML = '<img src="./assets/images/other/loader.gif" class="card-img-top rounded" alt="loading">'
