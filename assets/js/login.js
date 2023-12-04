4380f59e0ebe08c83c681f73fc70c24a5d4bdaef
Xudaverdi <xudaverdi.sh@gmail.com>
2023-12-05 02:34:02 +0400

    create account ready

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,43 +1,59 @@
 
 const formContainer = document.querySelector('.formContainer');
 
+const createloginUi = () => {
+    formContainer.innerHTML = `   
+    <div class='me-3 w-100 my-2'>
+        <h3 class="ms-2">Log in to Exclusive</h3>
+        <p class="ms-3">Enter your details below</p>
+        <form data-form='login' class="inputsAndBtns" name="form">
+            <div class="inputs d-flex flex-column position-relative">
+                <input data-email="emailLogin" name="email" type="text" class="borderlessInput mt-3 mb-1 py-2 ps-1" placeholder="Email or Phone Number">
+                <div id="email_error" class='fs-12 text-danger bg-light-red rounded px-2 d-none'>Please fill up your e-mail or phone number</div>
+                <input id="inputPassLoginPage" name="password" type="password" class="borderlessInput mt-1 mb-1 w-100 py-2 ps-1" placeholder="Password">
+                <i id="eyeIconLogin" class="position-absolute fs-5 text-secondary fa-solid fa-eye-slash"></i>
+                <div id="pass_error" class='fs-12 text-danger bg-light-red rounded px-2 d-none'>Must be only numbers, lowercase and uppercase letters, and total  characters at least 8.</div>
+                <div class="d-flex align-items-center justify-content-between container mt-3">
+                    <button id='createBtn' type='submit' class="button createBtn my-1">Log in</button>
+                    <a class='text-light-orange ' href='#'>Forget Password?</a>
+                </div>
+            </div>
+        </form>
+    </div>`;
+}
+const helperForLoginFunc = () => {
+    createloginUi()
+    showAndHidePassword("eyeIconLogin", "inputPassLoginPage")
+    // forum validation
+    const selectors = {
+        form: document.forms['form'],
+        email: document.forms['form']['email'],
+        pass: document.forms['form']['password'],
+        pass_error: document.getElementById('pass_error'),
+        email_error: document.getElementById('email_error')
+    };
+    selectElement(selectors.form, selectors.email, selectors.email_error, selectors.pass, selectors.pass_error)
+}
+
 const loginPage = () => {
     if (formContainer) {
-        document.getElementById('logIn').addEventListener('click', (event) => {
-            event.preventDefault();
+        const login = document.getElementById('logIn')
+        const sigin = document.getElementById('createBtn')
+        if (login) {
+            login.addEventListener('click', (event) => {
+                event.preventDefault();
+                helperForLoginFunc()
 
-            formContainer.innerHTML = `   
-            <div class='me-3 w-100 my-2'>
-                <h3 class="ms-2">Log in to Exclusive</h3>
-                <p class="ms-3">Enter your details below</p>
-                <form class="inputsAndBtns" name="form">
-                    <div class="inputs d-flex flex-column position-relative">
-                        <input data-email="emailLogin" name="email" type="text" class="borderlessInput mt-3 mb-1 py-2 ps-1" placeholder="Email or Phone Number">
-                        <div id="email_error" class='fs-12 text-danger bg-light-red rounded px-2 d-none'>Please fill up your e-mail or phone number</div>
-                        <input id="inputPassLoginPage" name="password" type="password" class="borderlessInput mt-1 mb-1 w-100 py-2 ps-1" placeholder="Password">
-                        <i id="eyeIconLogin" class="position-absolute fs-5 text-secondary fa-solid fa-eye-slash"></i>
-                        <div id="pass_error" class='fs-12 text-danger bg-light-red rounded px-2 d-none'>Must be only numbers, lowercase and uppercase letters, and total  characters at least 8.</div>
-                        <div class="d-flex align-items-center justify-content-between container mt-3">
-                            <button type='submit' class="button createBtn my-1">Log in</button>
-                            <a class='text-light-orange ' href='#'>Forget Password?</a>
-                        </div>
-                    </div>
-                </form>
-            </div>`;
-            showAndHidePassword("eyeIconLogin", "inputPassLoginPage")
-            // forum validation
-            const selectors = {
-                form: document.forms['form'],
-                email: document.forms['form']['email'],
-                pass: document.forms['form']['password'],
-                pass_error: document.getElementById('pass_error'),
-                email_error: document.getElementById('email_error')
-            };
+            });
+        } else {
+            sigin.addEventListener('click', (event) => {
+                event.preventDefault();
+                createloginUi()
+                helperForLoginFunc()
 
+            });
+        }
 
-            selectElement(selectors.form, selectors.email, selectors.email_error, selectors.pass, selectors.pass_error)
-
-        });
     }
 };
 
@@ -57,7 +73,7 @@ function showAndHidePassword(eyeIconId, passInputId) {
             icon.classList.add('fa-solid', addClass);
         });
     }
-
+    return true
 }
 
 loginPage();
@@ -82,10 +98,32 @@ function selectElement(form, email, email_error, pass, pass_error, nameInput, na
         nameInput.addEventListener('input', name_verify);
     }
     if (form) {
-        form.addEventListener('submit', (e) => {
-            e.preventDefault();
-            validateForm();
-        });
+        const id = form.getAttribute('data-form');
+        if (id == 'login') {
+            form.addEventListener('submit', (e) => {
+                e.preventDefault();
+                const value = validateForm();
+                if (value) {
+                    setSpinner()
+                }
+
+            });
+        } else {
+            document.querySelector('.signBtn').addEventListener('click', (e) => {
+                e.preventDefault();
+                const value = validateForm();
+                if (value) {
+                    createloginUi()
+
+                    const log = loginPage()
+                    if (log) {
+                        validateForm()
+                    }
+                }
+
+            })
+
+        }
     }
 
     function validateForm() {
@@ -104,6 +142,10 @@ function selectElement(form, email, email_error, pass, pass_error, nameInput, na
         }
 
 
+        return true
+    }
+
+    function setSpinner() {
         const value = Boolean(true);
         const queryString = new URLSearchParams(value).toString()
 
@@ -113,10 +155,9 @@ function selectElement(form, email, email_error, pass, pass_error, nameInput, na
             spinner.classList.add('d-none')
             window.location.href = "header.html?" + queryString;
         }, 3000)
-
-
-
     }
+
+
     // form input class changes
     function validateInput(input, errorElement, regex) {
         if (!regex.test(input.value)) {
