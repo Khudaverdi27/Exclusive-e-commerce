a21c09b963ebef50f071ceee1e282da39ee37b74
Xudaverdi <xudaverdi.sh@gmail.com>
2023-12-12 17:27:08 +0400

    merge problems solved

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
+import "./navbar.js";
+import "./counter.js";
+import "./displayProducts.js";
+import "./workers.js";
+import "./statistic.js";
+import "./login.js";
+import "./wishlist.js";
+import "./productDetails.js";
+import "./spinner.js";
+import "./footer.js";
+import "./contact.js";
 
