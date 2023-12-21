6d945ba89bed951467da2d4bb05e0a02fbb00429
Xudaverdi <xudaverdi.sh@gmail.com>
2023-12-21 15:52:58 +0400

    working on product details

--- assets/js/productDetails.js
+++ assets/js/productDetails.js
@@ -31,32 +31,34 @@ if (ui.plusBtn) {
 
 const detailFromStorage = JSON.parse(getStorage("details")) || [];
 
-document.getElementById("detail--name").textContent = detailFromStorage.name
-document.getElementById("detail--title").textContent = detailFromStorage.name
-document.getElementById("detail-category").textContent = detailFromStorage.category + "/"
-document.getElementById("detail--desc").textContent = detailFromStorage.description
-document.getElementById("detail--price").textContent = "$" + detailFromStorage.price + ".00"
-document.querySelectorAll(".img-w").forEach(img => {
-    img.src = detailFromStorage.image
-})
-document.querySelectorAll(".slide-img").forEach(img => {
-    img.src = detailFromStorage.image
-})
+const setElementText = (elementId, text) => document.getElementById(elementId).textContent = text;
+
+const setElementSrc = (selector, src) => document.querySelectorAll(selector).forEach(el => el.src = src);
+
 const sizeContainer = document.getElementById("sizeContainer");
 
-if (detailFromStorage.size) {
-    const sizeHTML = `
-    <span class="fs-4">Size:</span>
-    <button class="btn btn-sm border border-2 box-btn fw-bold">XS</button>
-    <button class="btn btn-sm border border-2 box-btn fw-bold mx-1">S</button>
-    <button class="btn btn-sm border border-2 box-btn fw-bold">M</button>
-    <button class="btn btn-sm border border-2 box-btn fw-bold mx-1">L</button>
-    <button class="btn btn-sm border border-2 box-btn fw-bold">XL</button>
-  `;
+if (sizeContainer) {
+    setElementText("detail--name", detailFromStorage.name);
+    setElementText("detail--title", detailFromStorage.name);
+    setElementText("detail-category", detailFromStorage.category + "/");
+    setElementText("detail--desc", detailFromStorage.description);
+    setElementText("detail--price", "$" + detailFromStorage.price + ".00");
+
+    setElementSrc(".img-w", detailFromStorage.image);
+    setElementSrc(".slide-img", detailFromStorage.image);
+
+    const sizeHTML = detailFromStorage.size ? `
+        <span class="fs-4">Size:</span>
+        <button class="btn btn-sm border border-2 box-btn fw-bold">XS</button>
+        <button class="btn btn-sm border border-2 box-btn fw-bold mx-1">S</button>
+        <button class="btn btn-sm border border-2 box-btn fw-bold">M</button>
+        <button class="btn btn-sm border border-2 box-btn fw-bold mx-1">L</button>
+        <button class="btn btn-sm border border-2 box-btn fw-bold">XL</button>
+    ` : "";
+
     sizeContainer.innerHTML = sizeHTML;
-} else {
-    sizeContainer.innerHTML = "";
 }
+
 
 
 
