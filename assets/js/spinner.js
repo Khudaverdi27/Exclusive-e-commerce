9bebf17f70f8077ee8c18a968529c9e69894fee5
Xudaverdi <xudaverdi.sh@gmail.com>
2024-01-07 23:56:55 +0400

    spinner background was blured

--- assets/js/spinner.js
+++ assets/js/spinner.js
@@ -1,5 +1,5 @@
 const spinner = document.querySelector('.loader-card')
-spinner.innerHTML = '<img src="./assets/images/other/loader.gif" class="card-img-top rounded" alt="loading">'
+spinner.innerHTML = '<img src="./assets/images/other/loader.gif" class="box-spinner card-img-top rounded" alt="loading">'
 
 function loading() {
     spinner.classList.remove('d-none')
